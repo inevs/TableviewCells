e5d0263b40dd2910af90b88c6868ed19dacf0922
Sven Günther <sven.guenther@it-agile.de>
2015-02-16 12:25:47 +0100

    Initial Commit

---
+++ TableviewCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableviewCells
+//
+//  Created by Sven Günther on 16.02.15.
+//  Copyright (c) 2015 shg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
